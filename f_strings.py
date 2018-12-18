78ddf525d9dccaee6af7e0feb747473bc664917d
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2018-12-18 11:42:47 +0200

    12/18/18

---
+++ f_strings.py
@@ -0,0 +1,32 @@
+name = "Eric"
+profession = "comedian"
+affiliation = "Monty Python"
+
+message = (
+    f"Hi {name}.\n"
+    f"You are a {profession}.\n"
+    f"You were in {affiliation}.\n"
+)
+
+print(message)
+
+# class Comedian:
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+#
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name} is {self.age}."
+#
+#     def __repr__(self):
+#         return f"{self.first_name} {self.last_name} is {self.age}. Surprise!"
+#
+#
+# new_comedian = Comedian("Eric", "Idle", "74")
+# print(f"{new_comedian}")
+
+# name = "Eric"
+# age = 74
+#
+# print(f"Hello, {name}. You are {age}.")
--- range_p.py
+++ range_p.py
@@ -1,4 +1,52 @@
-captains = ['Janeway', 'Picard', 'Sisko']
+import numpy as np
 
-for captain in captains:
-    print(captain)
+
+array = np.linspace(1, 4, 20)
+print(array)
+# for i in np.arange(0.3, 1.6, 0.3):
+#     print(i)
+
+# array = np.arange(0.3, 1.6, 0.3)
+# print(array)
+
+# for i in range(3.3):
+#     print(i)
+
+# for i in reversed(range(5)):
+#     print(i)
+
+# for i in range(10, -6, -2):
+#     print(i)
+
+# for i in range(3, 100, 25):
+#     print(i)
+
+# for i in range(3, 16, 3):
+#     quotient = i / 3
+#     print(f"{i} division by 3, the result {int(quotient)}.")
+
+# for i in range(1, 8):
+#     print(i)
+
+# for i in range(3):
+#     print(i)
+
+# captains = ['Janeway', 'Picard', 'Sisko']
+#
+# for i in range(len(captains)):
+#     print(captains[i])
+
+# for i in range(3, 16, 3):
+#     quotient = 1 / 3
+#     print(f"{i} division by 3, the result {int(quotient)}.")
+
+# numbers_divisible_by_three = [3, 6, 9, 12, 15]
+#
+# for num in numbers_divisible_by_three:
+#     quotient = num / 3
+#     print(f"{num} division on 3, the result {int(quotient)}.")
+#
+# captains = ['Janeway', 'Picard', 'Sisko']
+#
+# for captain in captains:
+#     print(captain)
